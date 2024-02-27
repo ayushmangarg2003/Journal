b84fefd031ee9409d3b18f7377c8bd7832d9228e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-02-27 11:02:08 +0530

    Title Changed

--- client/src/assets/data/utils.js
+++ client/src/assets/data/utils.js
@@ -9,7 +9,7 @@ export const COPYRIGHT = "Copyright @ All Rights Reserved";
 export const HISTORY = "The journal MAIT Journal of Science and Technology, published by Maharaja Agrasen University Publications, New Delhi, will provide a forum for publishing original contributions in the field of science and technology that also includes comprehensive technical review articles in interdisciplinary domains and original research papers in all important engineering topics. The journal encourages all experienced and qualified researchers, professionals, and other stakeholders to share their knowledge and experiences. The journal is published Annually. The first issue of the journal is likely to be released very soon.";
 export const MISSION = "It aims to be a leading peer reviewed platform and an authorized source of information for analyses, reviews and evaluations related to all branches of science and engineering. Papers are invited to investigate the prospects of novel technologies, devices, systems, materials, processes, operation, performance, maintenance and control in the field of science and technologies. It is a multidisciplinary Journal, which includes all areas of Science and Technology. ";
 export const VISION = "The Journal papers are available to eminent scholars, students, faculties, scientists, and corporate sectors. We will provide free PDF and access to authors. This is a free Journal, i.e. no publication fee.";
-export const HOME_HEAD = "One Stop For Quality Open Access Publishing";
+export const HOME_HEAD = "Welcome To MAIT Journal";
 export const HOME_DESC = "Exploring Frontiers, Inspiring Minds: Your Gateway to Knowledge and Discovery.";
 
 export const YEARS = "130+ ";
